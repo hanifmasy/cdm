4a8a307f0e39e50bed4db5a837c6ea3bb184814b
nones5 <amir.makmur32@gmail.com>
2021-07-26 14:25:31 +0800

    Update Sfgopro-accept-offer

--- resources/views/admin/reportCustomer/sfgopro/accept.blade.php
+++ resources/views/admin/reportCustomer/sfgopro/accept.blade.php
@@ -36,14 +36,14 @@
                                     <th>Status</th>
                                     <th>Order Status</th>
                                     <th>SC Number</th>
-									<th>Message</th>
-									<th>Source</th>
-									<th>Source Phone</th>
-									<th>Attachment</th>
-									<th>Created at</th>
-									<th>Updated at</th>
-									<th>Update Time</th>
-									<th>Primary Key</th>
+                                    <th>Message</th>
+                                    <th>Source</th>
+                                    <th>Source Phone</th>
+                                    <th>Attachment</th>
+                                    <th>Created at</th>
+                                    <th>Updated at</th>
+                                    <th>Update Time</th>
+                                    <th>Primary Key</th>
                                 </tr>
                             </thead>
                         </table>
@@ -52,110 +52,111 @@
             </div>
         </div>
     </div>
-    @endsection
-    @section('scripts')
-    <script>
-        $(function() {
-            let url = window.location.html;
-            let dtOverrideGlobals = {
-                processing: true,
-                serverSide: true,
-                retrieve: true,
-                aaSorting: [],
-                ajax: url,
-                columns: [{
-                        data: 'DT_RowIndex',
-                        orderable: false,
-                        searchable: false
-                    },
-                    {
-                        data: 'id',
-                        name: 'id'
-                    },
-                    {
-                        data: 'customer_id',
-                        name: 'customer_id'
-                    },
-                    {
-                        data: 'seller_id',
-                        name: 'seller_id'
-                    },
-                    {
-                        data: 'from_package',
-                        name: 'from_package'
-                    },
-                    {
-                        data: 'from_price',
-                        name: 'from_price'
-                    },
-                    {
-                        data: 'offer_type',
-                        name: 'offer_type'
-                    },
-                    {
-                        data: 'offer_subtype',
-                        name: 'offer_subtype'
-                    },
-                    {
-                        data: 'offer_price',
-                        name: 'offer_price'
-                    },
-                    {
-                        data: 'status',
-                        name: 'status'
-                    },
-                    {
-                        data: 'order_status',
-                        name: 'order_status'
+</div>
+@endsection
+@section('scripts')
+<script>
+    $(function() {
+        let url = window.location.html;
+        let dtOverrideGlobals = {
+            processing: true,
+            serverSide: true,
+            retrieve: true,
+            aaSorting: [],
+            ajax: url,
+            columns: [{
+                    data: 'DT_RowIndex',
+                    orderable: false,
+                    searchable: false
+                },
+                {
+                    data: 'id',
+                    name: 'id'
+                },
+                {
+                    data: 'customer_id',
+                    name: 'customer_id'
+                },
+                {
+                    data: 'seller_id',
+                    name: 'seller_id'
+                },
+                {
+                    data: 'from_package',
+                    name: 'from_package'
+                },
+                {
+                    data: 'from_price',
+                    name: 'from_price'
+                },
+                {
+                    data: 'offer_type',
+                    name: 'offer_type'
+                },
+                {
+                    data: 'offer_subtype',
+                    name: 'offer_subtype'
+                },
+                {
+                    data: 'offer_price',
+                    name: 'offer_price'
+                },
+                {
+                    data: 'status',
+                    name: 'status'
+                },
+                {
+                    data: 'order_status',
+                    name: 'order_status'
 
-                    },
-                    {
-                        data: 'sc_number',
-                        name: 'sc_number'
-                    },
-					{
-                        data: 'message',
-                        name: 'message'
-                    },
-					{
-                        data: 'source',
-                        name: 'source'
-                    },
-					{
-                        data: 'source_phone',
-                        name: 'source_phone'
-                    },
-					{
-                        data: 'attachment',
-                        name: 'attachment'
-                    },
-					{
-                        data: 'created_at',
-                        name: 'created_at'
-                    },
-					{
-                        data: 'updated_at',
-                        name: 'updated_at'
-                    },
-					{
-                        data: 'updatetime',
-                        name: 'updatetime'
-                    },
-					{
-                        data: 'primarykey',
-                        name: 'primarykey'
-                    },
-                ],
-                orderCellsTop: true,
-                order: [
-                    [1, 'desc']
-                ],
-                pageLength: 50,
-            };
-            $('#kt_datatable_search').keyup(function() {
-                table.search($(this).val()).draw();
-            });
-            let table = $('#order-listing').DataTable(dtOverrideGlobals);
+                },
+                {
+                    data: 'sc_number',
+                    name: 'sc_number'
+                },
+                {
+                    data: 'message',
+                    name: 'message'
+                },
+                {
+                    data: 'source',
+                    name: 'source'
+                },
+                {
+                    data: 'source_phone',
+                    name: 'source_phone'
+                },
+                {
+                    data: 'attachment',
+                    name: 'attachment'
+                },
+                {
+                    data: 'created_at',
+                    name: 'created_at'
+                },
+                {
+                    data: 'updated_at',
+                    name: 'updated_at'
+                },
+                {
+                    data: 'updatetime',
+                    name: 'updatetime'
+                },
+                {
+                    data: 'primarykey',
+                    name: 'primarykey'
+                },
+            ],
+            orderCellsTop: true,
+            order: [
+                [1, 'desc']
+            ],
+            pageLength: 50,
+        };
+        $('#kt_datatable_search').keyup(function() {
+            table.search($(this).val()).draw();
         });
-    </script>
-    @endsection
+        let table = $('#order-listing').DataTable(dtOverrideGlobals);
+    });
+</script>
+@endsection
